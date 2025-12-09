49d6c3d1ea5b549a24d2a2e36073a5d4b3b2ad42
NappoN49 <parawin39228@gmail.com>
2025-12-09 15:10:36 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int sumeven=0,sumodd=0,a;
+    cout << "Enter an integer: ";cin>>a;
+    while(a!=0){
+        if(a%2==0){sumeven++;}
+        else{sumodd++;}
+        cout << "Enter an integer: ";cin>>a;
+    }
+    cout << "#Even numbers = " << sumeven << "\n";
+    cout << "#Odd numbers = " << sumodd;
     return 0;
 }
