b5970b9d2086a84bf0a22af9c0cb5973a8e52dc2
NappoN49 <parawin39228@gmail.com>
2025-12-09 15:33:27 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
-
+#include<math.h>
 using namespace std;
+
+double deg2rad(double Deg){
+    return Deg*(M_PI/180.0);
+}
+
+double rad2deg(double Rad){
+    return Rad*(180.0/M_PI);
+}
+
+double findXComponent(double lenA,double lenB,double degA,double degB){
+    return (lenA*cos(degA))+(lenB*cos(degB));
+}
+
+double findYComponent(double lenA,double lenB,double degA,double degB){
+    return (lenA*sin(degA))+(lenB*sin(degB));
+}
+
+double pythagoras(double A,double B){
+    return sqrt(pow(A,2)+pow(B,2));
+}
+
+void showResult(double ANSWER1,double ANSWER2){
+    cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout<<"Length of the resultant vector = "<<ANSWER1<<"\n";
+    cout<<"Direction of the resultant vector (deg) = "<<ANSWER2<<"\n";
+    cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"; 
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
